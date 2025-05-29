42617ee944eace68d983d919ecfb121a05a402de
pearmeow <perryhuang2015@gmail.com>
2025-05-29 09:16:08 -0400

    Complete exercise 9: sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start = 0, end = 0) {
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  if (start % 1 !== 0 || end % 1 !== 0) return "ERROR";
+  if (start > end) {
+    let temp = end
+    end = start;
+    start = temp;
+  }
+  return (start + end) * (end - start + 1) / 2;
 };
 
 // Do not edit below this line
