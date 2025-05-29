bd41f762bcf70ab5a5b1c533065257f3d5da9b5f
pearmeow <perryhuang2015@gmail.com>
2025-05-29 09:09:30 -0400

    Complete exercise 8: remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr = []) {
+  for (let i = 1; i < arguments.length; ++i) {
+    let index = -1;
+    do {
+      index = arr.indexOf(arguments[i]);
+      if (index != -1) arr.splice(index, 1);
+    } while (index != -1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
