59ffb29eb3e8d10a57b3de9c8a3c295eb577f692
pearmeow <perryhuang2015@gmail.com>
2025-05-29 08:51:15 -0400

    Complete exercise 7: reverse a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str = "") {
+  arr = str.split("");
+  newStr = "";
+  for (let i = arr.length - 1; i > -1; --i) {
+    newStr += arr[i];
+  }
+  return newStr;
 };
 
 // Do not edit below this line
