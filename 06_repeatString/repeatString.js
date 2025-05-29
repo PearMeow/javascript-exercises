8e453016d5e1d4b0b2e6b6a4c569b7fe163796c2
pearmeow <perryhuang2015@gmail.com>
2025-05-29 08:46:05 -0400

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str = "", reps = 0) {
+  if (reps < 0) return "ERROR";
+  let res = "";
+  for (let i = 0; i < reps; ++i) res += str;
+  return res;
 };
 
 // Do not edit below this line
