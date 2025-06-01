925b515bb247457ad22eaf5c37c10b3dd58eed96
pearmeow <perryhuang2015@gmail.com>
2025-06-01 14:15:00 -0400

    Finish all js exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people = []) {
+  let thePerson;
+  let maxAge = 0;
+  for (let person of people) {
+    if (person.yearOfDeath === undefined) {
+      if (maxAge < 2026 - person.yearOfBirth) thePerson = person;
+      maxAge = maxAge > 2026 - person.yearOfBirth ? maxAge : 2026 - person.yearOfBirth;
+    } else {
+      if (maxAge < person.yearOfDeath - person.yearOfBirth) thePerson = person;
+      maxAge = maxAge > person.yearOfDeath - person.yearOfBirth ? maxAge : person.yearOfDeath - person.yearOfBirth;
+    }
+  }
+  return thePerson;
 };
 
 // Do not edit below this line
