4fe71ed1c829618d05daf51a16fcacefd3919a48
pearmeow <perryhuang2015@gmail.com>
2025-06-01 11:30:45 -0400

    Finish fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+  nth = +nth;
+  if (nth < 0) return "OOPS";
+  let fib = [0, 1];
+  while (fib.length < nth + 1) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return fib[nth];
 };
 
 // Do not edit below this line
