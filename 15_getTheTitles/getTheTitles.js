8d2509f49121a8ed6cd4305034167e05cab1b038
pearmeow <perryhuang2015@gmail.com>
2025-06-01 12:06:35 -0400

    Finish get the titles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr = []) {
+  return arr.reduce((list, curr) => {
+    list.push(curr.title);
+    return list;
+  }, []);
 };
 
 // Do not edit below this line
